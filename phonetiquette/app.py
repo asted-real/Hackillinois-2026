a6aade80629658645fe17c7e9a1b179f51a1e837
emilyem555 <mastersemily518@gmail.com>
2026-02-28 16:50:50 -0600

    changed some things

--- phonetiquette/app.py
+++ phonetiquette/app.py
@@ -1,16 +1,18 @@
 import eng_to_ipa as ipa
 import word_funcs as wf
+from flask_cors import CORS
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+CORS(app) 
 
 
-
-
-@app.route('/word_gen')
-def word_gen(diff):
-	return { 'word': wf.random_word(diff) } 
+@app.route('/word_gen', methods=['POST'])
+def word_gen():
+    data = request.get_json()
+    diff = data.get("difficulty", 0)
+    return jsonify({ 'word': wf.random_word(diff) })
 
 @app.route('/check_answer')
 def check_answer(response,english):
@@ -19,3 +21,6 @@ def check_answer(response,english):
 @app.route('/correct_answer')
 def correct_answer(word):
 	return {'answer_list':ipa.convert(word,keep_punct=False,stress_marks=False,retrieve_all=True)}
+
+if __name__ == '__main__':
+	app.run()
