f681a6bcf65f4932709de34054e29073135c6c1d
emilyem555 <mastersemily518@gmail.com>
2026-02-28 01:07:00 -0600

    finish gradient

--- IPA-site/src/App.js
+++ IPA-site/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-          Learn React
+          <h1 className="phoneticate">Phonetiquette</h1>
     </div>
   );
 }
