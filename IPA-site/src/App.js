cf7b0fba83c83728737a7c1fe3274123da534640
emilyem555 <mastersemily518@gmail.com>
2026-02-28 00:32:18 -0600

    started react app

---
+++ IPA-site/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          Learn React
+    </div>
+  );
+}
+
+function Keyboard(){
+
+}
+
+function generated_words() { 
+
+}
+
+export default App;
