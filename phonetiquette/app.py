cc2789bbe70325e8f59f1041612d337c7bf15b16
asted-real <asted338@gmail.com>
2026-02-28 18:44:26 -0600

    linked the front & back!?!?

--- phonetiquette/app.py
+++ phonetiquette/app.py
@@ -1,23 +1,40 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import word_funcs as wf
-
+import eng_to_ipa as ipa
 
 from flask import Flask
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/word_gen')
-def word_gen(diff):
-	return jsonify({ 'word': wf.random_word(diff) }) 
+@app.route('/word_gen',methods=['POST'])
+def word_gen():
+	data = request.get_json()
+	diff = data.get("difficulty",0) # default to 0 if missing
+	word = wf.random_word(diff)
+	return jsonify({ 'word': word}) 
 
 @app.route('/check_answer', methods=['POST'])
-def check_answer(response,english):
-	return jsonify({ 'feedback': wf.correct_check(response,english)})
+def check_answer():
+	data = request.get_json()
+	response = data.get("response", "")
+	english = data.get("english", "")
+	feedback = wf.correct_check(response, english)
+	return jsonify({ 'feedback': feedback})
 
-@app.route('/correct_answer')
-def correct_answer(word):
-	return {'answer_list':ipa.convert(word,keep_punct=False,stress_marks=False,retrieve_all=True)}
+@app.route('/correct_answer',methods=['POST'])
+def correct_answer():
+	data = request.get_json()
+	word = word = data.get("word", "")
+	return jsonify({
+        'answer_list': ipa.convert(
+            word,
+            keep_punct=False,
+            stress_marks=False,
+            retrieve_all=True
+        )
+    })
 
 if __name__ == '__main__':
 	app.run()
